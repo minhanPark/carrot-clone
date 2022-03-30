93abc7cc02b727f77ac51679c9f53450fa79f7fa
박민한 <alsgks1218@naver.com>
2022-03-30 17:51:59 +0900

    add many pages

---
+++ pages/chats/[id].tsx
@@ -0,0 +1,143 @@
+import type { NextPage } from "next";
+
+const ChatDetail = () => {
+  return (
+    <div className="py-10 px-4 space-y-4">
+      <div className="flex items-center space-x-2">
+        <div className="w-8 h-8 rounded-full bg-slate-300" />
+        <div className="w-3/4 text-sm text-gray-700 flex justify-start">
+          <p className="p-2 border rounded-md inline-block border-gray-300">
+            Hi how much are you selling them forsssssssssssssssssssssssssssss?
+          </p>
+        </div>
+      </div>
+      <div className="flex items-center space-x-2 space-x-reverse flex-row-reverse">
+        <div className="w-8 h-8 rounded-full bg-slate-300" />
+        <div className="w-3/4 text-sm text-gray-700 flex justify-end">
+          <p className="p-2 border rounded-md inline-block border-gray-300">
+            I want ￦20,000sdsdsdsdsdsdsd
+          </p>
+        </div>
+      </div>
+      <div className="flex items-center space-x-2">
+        <div className="w-8 h-8 rounded-full bg-slate-300" />
+        <div className="w-3/4 text-sm text-gray-700 flex justify-start">
+          <p className="p-2 border rounded-md inline-block border-gray-300">
+            미쳤어
+          </p>
+        </div>
+      </div>
+      <div className="flex items-center space-x-2">
+        <div className="w-8 h-8 rounded-full bg-slate-300" />
+        <div className="w-3/4 text-sm text-gray-700 flex justify-start">
+          <p className="p-2 border rounded-md inline-block border-gray-300">
+            Hi how much are you selling them forsssssssssssssssssssssssssssss?
+          </p>
+        </div>
+      </div>
+      <div className="flex items-center space-x-2 space-x-reverse flex-row-reverse">
+        <div className="w-8 h-8 rounded-full bg-slate-300" />
+        <div className="w-3/4 text-sm text-gray-700 flex justify-end">
+          <p className="p-2 border rounded-md inline-block border-gray-300">
+            I want ￦20,000sdsdsdsdsdsdsd
+          </p>
+        </div>
+      </div>
+      <div className="flex items-center space-x-2">
+        <div className="w-8 h-8 rounded-full bg-slate-300" />
+        <div className="w-3/4 text-sm text-gray-700 flex justify-start">
+          <p className="p-2 border rounded-md inline-block border-gray-300">
+            미쳤어
+          </p>
+        </div>
+      </div>
+      <div className="flex items-center space-x-2">
+        <div className="w-8 h-8 rounded-full bg-slate-300" />
+        <div className="w-3/4 text-sm text-gray-700 flex justify-start">
+          <p className="p-2 border rounded-md inline-block border-gray-300">
+            Hi how much are you selling them forsssssssssssssssssssssssssssss?
+          </p>
+        </div>
+      </div>
+      <div className="flex items-center space-x-2 space-x-reverse flex-row-reverse">
+        <div className="w-8 h-8 rounded-full bg-slate-300" />
+        <div className="w-3/4 text-sm text-gray-700 flex justify-end">
+          <p className="p-2 border rounded-md inline-block border-gray-300">
+            I want ￦20,000sdsdsdsdsdsdsd
+          </p>
+        </div>
+      </div>
+      <div className="flex items-center space-x-2">
+        <div className="w-8 h-8 rounded-full bg-slate-300" />
+        <div className="w-3/4 text-sm text-gray-700 flex justify-start">
+          <p className="p-2 border rounded-md inline-block border-gray-300">
+            미쳤어
+          </p>
+        </div>
+      </div>
+      <div className="flex items-center space-x-2">
+        <div className="w-8 h-8 rounded-full bg-slate-300" />
+        <div className="w-3/4 text-sm text-gray-700 flex justify-start">
+          <p className="p-2 border rounded-md inline-block border-gray-300">
+            Hi how much are you selling them forsssssssssssssssssssssssssssss?
+          </p>
+        </div>
+      </div>
+      <div className="flex items-center space-x-2 space-x-reverse flex-row-reverse">
+        <div className="w-8 h-8 rounded-full bg-slate-300" />
+        <div className="w-3/4 text-sm text-gray-700 flex justify-end">
+          <p className="p-2 border rounded-md inline-block border-gray-300">
+            I want ￦20,000sdsdsdsdsdsdsd
+          </p>
+        </div>
+      </div>
+      <div className="flex items-center space-x-2">
+        <div className="w-8 h-8 rounded-full bg-slate-300" />
+        <div className="w-3/4 text-sm text-gray-700 flex justify-start">
+          <p className="p-2 border rounded-md inline-block border-gray-300">
+            미쳤어
+          </p>
+        </div>
+      </div>
+      <div className="flex items-center space-x-2">
+        <div className="w-8 h-8 rounded-full bg-slate-300" />
+        <div className="w-3/4 text-sm text-gray-700 flex justify-start">
+          <p className="p-2 border rounded-md inline-block border-gray-300">
+            Hi how much are you selling them forsssssssssssssssssssssssssssss?
+          </p>
+        </div>
+      </div>
+      <div className="flex items-center space-x-2 space-x-reverse flex-row-reverse">
+        <div className="w-8 h-8 rounded-full bg-slate-300" />
+        <div className="w-3/4 text-sm text-gray-700 flex justify-end">
+          <p className="p-2 border rounded-md inline-block border-gray-300">
+            I want ￦20,000sdsdsdsdsdsdsd
+          </p>
+        </div>
+      </div>
+      <div className="flex items-center space-x-2">
+        <div className="w-8 h-8 rounded-full bg-slate-300" />
+        <div className="w-3/4 text-sm text-gray-700 flex justify-start">
+          <p className="p-2 border rounded-md inline-block border-gray-300">
+            미쳤어
+          </p>
+        </div>
+      </div>
+      <div className="fixed w-full bottom-2 mx-auto max-w-md inset-x-0">
+        <div className="flex items-center relative">
+          <input
+            type="text"
+            className="shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none focus:border-orange-500 pr-12"
+          />
+          <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
+            <span className="flex items-center bg-orange-500 rounded-full px-3 test-sm text-white hover:bg-orange-600 cursor-pointer focus:ring-2 focus:ring-offset-2 focus:ring-orange-500">
+              &rarr;
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatDetail;
