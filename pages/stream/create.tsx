1dcb7b716cde7c05247a7f1f1ac531fd9d2e8abc
박민한 <alsgks1218@naver.com>
2022-04-01 10:19:05 +0900

    divided component

--- pages/stream/create.tsx
+++ pages/stream/create.tsx
@@ -1,49 +1,26 @@
 import type { NextPage } from "next";
+import Button from "../../components/button";
+import Input from "../../components/input";
+import Layout from "../../components/layout";
+import TextArea from "../../components/textarea";
 
 const Create: NextPage = () => {
   return (
-    <div className="px-4 py-16">
-      <div className="my-5">
-        <label htmlFor="productName" className="text-sm font-medium">
-          Name
-        </label>
-        <div className="rounded-md shadow-sm flex items-center mb-4">
-          <input
-            id="productName"
-            type="text"
-            placeholder="Product Name"
-            className="py-2 px-1 pl-4 border border-gray-300 appearance-none rounded-l-none w-full rounded-md placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-          />
-        </div>
-        <label htmlFor="price" className="text-sm font-medium">
-          Price
-        </label>
-        <div className="rounded-md relative shadow-sm flex items-center">
-          <div className="absolute left-0 pl-3 flex items-center justify-center pointer-events-none">
-            <span className="text-gray-500 text-sm">$</span>
-          </div>
-          <input
-            id="price"
-            type="text"
-            placeholder="0.00"
-            className="py-2 px-4 pl-7 border border-gray-300 appearance-none rounded-l-none w-full rounded-md placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-          />
-          <div className="absolute right-0 pr-3 flex items-center pointer-events-none">
-            <span className="text-gray-500">USD</span>
-          </div>
-        </div>
-      </div>
-      <div>
-        <label className="mb-1 block text-sm font-medium">Description</label>
-        <textarea
-          rows={4}
-          className="mt-1 shadow-sm w-full focus:ring-2 focus:outline-none focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500"
+    <Layout canGoBack title="Go Live">
+      <form className=" space-y-4 py-10 px-4">
+        <Input required label="Name" name="name" type="text" />
+        <Input
+          required
+          label="Price"
+          placeholder="0.00"
+          name="price"
+          type="text"
+          kind="price"
         />
-      </div>
-      <button className="mt-4 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
-        Go Live
-      </button>
-    </div>
+        <TextArea name="description" label="Description" />
+        <Button text="Go live" />
+      </form>
+    </Layout>
   );
 };
 
--- pages/stream/index.tsx
+++ pages/stream/index.tsx
@@ -1,4 +1,6 @@
 import type { NextPage } from "next";
+import Link from "next/link";
+import FloatingButton from "../../components/floating-button";
 import Layout from "../../components/layout";
 
 const Steam: NextPage = () => {
@@ -8,14 +10,16 @@ const Steam: NextPage = () => {
         {Array(4)
           .fill(1)
           .map((_, k) => (
-            <div key={k} className="pt-4 px-4">
-              <div className="w-full bg-slate-300 aspect-video rounded-md shadow-sm" />
-              <h3 className="font-medium text-gray-700 text-2xl mt-2">
-                아베베베벨ㄹ
-              </h3>
-            </div>
+            <Link key={k} href={`/stream/${k}`}>
+              <a className="pt-4 px-4 block">
+                <div className="w-full bg-slate-300 aspect-video rounded-md shadow-sm" />
+                <h3 className="font-medium text-gray-700 text-2xl mt-2">
+                  아베베베벨ㄹ
+                </h3>
+              </a>
+            </Link>
           ))}
-        <button className="fixed bottom-24 right-5 bg-orange-400 rounded-full p-4 text-white shadow-xl cursor-pointer hover:bg-orange-500 transition-colors">
+        <FloatingButton href="/stream/create">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-6 w-6"
@@ -30,7 +34,7 @@ const Steam: NextPage = () => {
               d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
             />
           </svg>
-        </button>
+        </FloatingButton>
       </div>
     </Layout>
   );
