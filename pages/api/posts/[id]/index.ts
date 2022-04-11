7b6e4041ca5df6f8adb2ac7ce6ad73a831027680
Running water <alsgks1218@naver.com>
2022-04-11 17:28:19 +0900

    add comment

--- pages/api/posts/[id]/index.ts
+++ pages/api/posts/[id]/index.ts
@@ -60,6 +60,7 @@ async function handler(
     post,
     isWondering,
   });
+  // 이렇게 find 하는 것도 아무것도 없으면 null이 갈건데 이때 프론트에서 상황에 맞게 처리해줘야함
 }
 
 export default withApiSession(withHandler({ methods: ["GET"], handler }));
