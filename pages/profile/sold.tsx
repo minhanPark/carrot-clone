3005f453bd57596e8f733b90ac34a20024843271
박민한 <alsgks1218@naver.com>
2022-04-14 17:40:00 +0900

    profile func

--- pages/profile/sold.tsx
+++ pages/profile/sold.tsx
@@ -1,3 +1,4 @@
+import ProductList from "@components/product-list";
 import type { NextPage } from "next";
 import Item from "../../components/item";
 import Layout from "../../components/layout";
@@ -5,17 +6,8 @@ import Layout from "../../components/layout";
 const Sold: NextPage = () => {
   return (
     <Layout title="판매내역" canGoBack>
-      <div className="flex flex-col space-y-5 pb-10  divide-y">
-        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-          <Item
-            id={i}
-            key={i}
-            title="iPhone 14"
-            price={99}
-            comments={1}
-            hearts={1}
-          />
-        ))}
+      <div className="flex flex-col space-y-5 pb-10  divide-y py-16">
+        <ProductList kind="sales" />
       </div>
     </Layout>
   );
