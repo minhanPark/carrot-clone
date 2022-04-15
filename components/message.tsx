f264951b8b8374b9779038422cd3d67ce57ea3df
박민한 <alsgks1218@naver.com>
2022-04-15 17:37:26 +0900

    add stream

--- components/message.tsx
+++ components/message.tsx
@@ -14,13 +14,15 @@ export default function Message({
   return (
     <div
       className={cls(
-        "flex  items-start",
+        "flex items-start",
         reversed ? "flex-row-reverse space-x-reverse" : "space-x-2"
       )}
     >
       <div className="w-8 h-8 rounded-full bg-slate-400" />
-      <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-        <p>{message}</p>
+      <div className="w-1/2 text-sm p-2 border border-gray-300 rounded-md">
+        <p className={cls(reversed ? "text-red-500" : "text-gray-700")}>
+          {message}
+        </p>
       </div>
     </div>
   );
