2491993bf12bae837c5536f83b6f4f1b12f90817
박민한 <alsgks1218@naver.com>
2022-04-25 13:38:57 +0900

    .

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+//images에 domian을 추가해야 Image 컴포넌트 쓸 때 에러 안남
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["imagedelivery.net"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
